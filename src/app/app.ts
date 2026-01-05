fd4a973d546eaf5ad31420f23af0cad4faedaf7a
tejus-dev <tejus2912@gmail.com>
2026-01-05 22:31:10 +0530

    navbar fixed for mobile

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,8 +4,17 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('clenstacky');
+  menuOpen = false;
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  closeMenu() {
+    this.menuOpen = false;
+  }
 }
