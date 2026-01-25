129549b5e55af9d2242e73dd6a75d828e9535a2a
tejus-dev <tejus2912@gmail.com>
2026-01-25 22:49:35 +0530

    revamped

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CardModule } from 'primeng/card';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
+  imports: [CardModule, CommonModule],
   styleUrls: ['./app.scss'],
 })
 export class App {
